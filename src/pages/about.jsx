21aa50ff36d37703a4b5de55700e552513ee8b1b
Ghostnodedev <vaibhavwalker14@gmail.com>
2026-08-24 16:10:52 +0530

    Add Privacy Policy link to navigation

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -125,6 +125,14 @@ function About() {
 
               </li>
 
+              <li className="nav-item">
+
+                <Link className="nav-link" to="/privacy-policy">
+                  Privacy Policy
+                </Link>
+
+              </li>
+
               <li className="nav-item ms-lg-3 mt-3 mt-lg-0">
 
                 <Link to="/contact">
