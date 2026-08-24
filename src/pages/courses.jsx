f1dedb55573d845dc2a182ad9bf97a8413bc15d5
Ghostnodedev <vaibhavwalker14@gmail.com>
2026-08-24 16:25:34 +0530

    Update courses.jsx

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -152,6 +152,14 @@ function Courses() {
                       </Link>
       
                     </li>
+
+                    <li className="nav-item">
+
+                <Link className="nav-link" to="/privacy-policy">
+                  Privacy Policy
+                </Link>
+
+              </li>
       
                     <li className="nav-item ms-lg-3 mt-3 mt-lg-0">
       
