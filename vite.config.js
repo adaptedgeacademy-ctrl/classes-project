4de72f37ea39cf5368e3e47bb5b97198cb99e938
Vinayak4243 <targetchamp4243@gmail.com>
2026-09-20 16:14:12 +0530

    Deploy site to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/classes-project/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
